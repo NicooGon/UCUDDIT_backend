31d103be04069150bddeac15e4baeb1baf9c56c1
NicooGon <nicolasgonzalezgutierrez2017@gmail.com>
2024-10-27 21:35:04 -0300

    first commit

---
+++ src/main/java/com/example/ucuddit/UcudditApplication.java
@@ -0,0 +1,13 @@
+package com.example.ucuddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcudditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcudditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ucuddit/UcudditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ucuddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcudditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
