339d5f2b8b6efc36922f005ed32fc9c438783de2
NicooGon <nicolasgonzalezgutierrez2017@gmail.com>
2024-11-07 23:19:58 -0300

    commentsLogic

---
+++ src/main/java/com/example/ucuddit/controller/CommentController.java
@@ -0,0 +1,28 @@
+package com.example.ucuddit.controller;
+
+import com.example.ucuddit.model.Comment;
+import com.example.ucuddit.service.CommentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping("/commentsByPost")
+    public List<Comment> getAllCommentsByPost(@RequestParam Integer postId) {
+        return commentService.getAllCommentsByPostId(postId);
+    }
+
+    @PostMapping("/comments")
+    public Comment saveComment(@RequestBody Comment comment) {
+        return commentService.saveComment(comment.getUser().getAuth0id(),comment.getPost().getPostId(), comment.getContent());
+    }
+}
---
+++ src/main/java/com/example/ucuddit/service/CommentService.java
@@ -0,0 +1,43 @@
+package com.example.ucuddit.service;
+
+import com.example.ucuddit.model.Comment;
+import com.example.ucuddit.model.Post;
+import com.example.ucuddit.model.User;
+import com.example.ucuddit.respository.CommentRepository;
+import com.example.ucuddit.respository.PostRepository;
+import com.example.ucuddit.respository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public CommentService(CommentRepository commentRepository, PostRepository postRepository, UserRepository userRepository) {
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<Comment> getAllCommentsByPostId(Integer postId) {
+        return commentRepository.findByPost_PostId(postId);
+    }
+
+    public Comment saveComment(String auth0id, Integer postId, String content) {
+        Post existingPost = postRepository.findBypostId(postId);
+        User existingUser = userRepository.findByauth0id(auth0id);
+
+        Comment newComment = new Comment();
+        newComment.setPost(existingPost);
+        newComment.setUser(existingUser);
+        newComment.setContent(content);
+        newComment.setCreationDate(LocalDate.now());
+
+        return commentRepository.save(newComment);
+    }
+}
